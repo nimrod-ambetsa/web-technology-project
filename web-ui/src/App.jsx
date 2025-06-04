25a0e43cee597c8f65067e03cf57d0ff252be950
Paddy-Nimrod <n.ambetsa21@gmail.com>
2025-06-04 21:36:31 +0300

    changes on frontend ui

--- web-ui/src/App.jsx
+++ web-ui/src/App.jsx
@@ -1,22 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import Header from './components/header'
+import "./App.css"
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
       <div>
+        <Header />
+        <div>
+          <div className="flex justify-center items-center h-96 bg-gradient-to-r from-blue-500 to-purple-500">
+            <div className="text-3xl font-bold text-slate-800">
+              Welcome to Empower Illiter
+            </div>
+            <div>
+              <button>list your service</button>
+              <button>Hire a service provider</button>
+            </div>
+          </div>
+          <div>
+            <div className='w-3/4 mx-auto my-10 p-5'>
+              <h2 className='font-bold text-2xl text-center mb-2'>About us</h2>
+              <div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero voluptate iusto corporis consectetur sed quam laborum ab eos,
+                  expedita ad similique consequatur obcaecati, distinctio culpa, cupiditate perferendis dolorum autem sunt. Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                  Vero voluptate iusto corporis consectetur sed quam laborum ab eos,
+                  expedita ad similique consequatur obcaecati, distinctio culpa, cupiditate perferendis dolorum autem sunt!</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero voluptate iusto corporis consectetur sed quam laborum ab eos,
+                  expedita ad similique consequatur obcaecati, distinctio culpa, cupiditate perferendis dolorum autem sunt!</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero voluptate iusto corporis consectetur sed quam laborum ab eos,
+                  expedita ad similique consequatur obcaecati, distinctio culpa, cupiditate perferendis dolorum autem sunt!</p>
+              </div>
+            </div>
+            <div className='w-3/4 mx-auto my-10 p-5'>
+              <h2 className='font-bold text-2xl text-center mb-2'>Our mission</h2>
+              <div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero voluptate iusto corporis consectetur sed quam laborum ab eos,
+                  expedita ad similique consequatur obcaecati, distinctio culpa, cupiditate perferendis dolorum autem sunt. Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                  Vero voluptate iusto corporis consectetur sed quam laborum ab eos,
+                  expedita ad similique consequatur obcaecati, distinctio culpa, cupiditate perferendis dolorum autem sunt!</p>
+              
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
 
-      </div>
-      <h1>sodel project</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
     </>
   )
 }
---
+++ web-ui/src/components/header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header() {
+    return (
+        <>
+            <div className="w-full bg-slate-900 text-white h-12 flex justify-start items-center">
+                <div className="text-xl mx-2">EMPOWER ILLITER</div>
+            </div>
+        </>
+    )
+}
